813477441fb77073879d0d89b791349ddddee6b3
Djalma  Bina <djbina2000@yahoo.com.br>
2017-12-10 17:25:42 -0200

    Update video-streaming.js

--- video-streaming.js
+++ video-streaming.js
@@ -4,15 +4,15 @@
  app.get('/', (req, res) => {
    fs.readFile('./index.html', (err, html) => res.end(html));
  });
- app.get('/movies/:movieName', (req, res) => {
+ app.get('/:movieName', (req, res) => {
    const { movieName } = req.params;
-   const movieFile = `./movies/${movieName}`;
+   const movieFile = `./${movieName}`;
    fs.stat(movieFile, (err, stats) => {
      if (err) {
        console.log(err);
        return res.status(404).end('<h1>Movie Not found</h1>');
      }
-     // Variáveis necessárias para montar o chunk header corretamente
+     // VariÃ¡veis necessÃ¡rias para montar o chunk header corretamente
      const { range } = req.headers;
      const { size } = stats;
      const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
@@ -25,13 +25,13 @@
        'Content-Length': chunkSize,
        'Content-Type': 'video/mp4'
      });
-     // É importante usar status 206 - Partial Content para o streaming funcionar
+     // Ã‰ importante usar status 206 - Partial Content para o streaming funcionar
      res.status(206);
      // Utilizando ReadStream do Node.js
-     // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
+     // Ele vai ler um arquivo e enviÃ¡-lo em partes via stream.pipe()
      const stream = fs.createReadStream(movieFile, { start, end });
      stream.on('open', () => stream.pipe(res));
      stream.on('error', (streamErr) => res.end(streamErr));
    });
  });
- app.listen(5000, () => console.log('VideoFlix Server!'));
+ app.listen(3000, () => console.log('VideoFlix Server!'));
